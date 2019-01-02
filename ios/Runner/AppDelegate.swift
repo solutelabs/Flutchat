f53bc2a56a34522fc4522c69d118ca0efdf73f4e
Smit Sonani <smitsonani@solutelabs.com>
2019-01-02 13:01:49 +0530

    Registered FlutterMethodChannel in iOS to open video chat feature

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,28 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let videoChatChannel = FlutterMethodChannel(name: "com.stl.flutchat/opentok",
+                                                    binaryMessenger: controller)
+        videoChatChannel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            switch call.method {
+            case "openVideoChat":
+                // TODO: Handle video chat related feature
+                break
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
 }
